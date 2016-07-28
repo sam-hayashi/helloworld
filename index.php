c03b441a889b8486faf3c22fe8a81e50e19140a2
mmasko <mymas81@gmail.com>
2016-07-28 15:46:41 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?> 
- <?php echo '<p>Hello uat1</p>'; ?>
- </body>
-</html>
+<?php
+
+echo "Hello World";
